81019f6becd244a51cdcbf046ff617b9b70dbcb6
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-10 19:19:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves (dwarf_names)# code an argument here
   # Your code here
+  dwarf_names.each_with_index do |i, index|
+     puts "#{index +1} #{i}"
+   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet (planet)# code an argument here
   # Your code here
+  planet.map do |i|
+    "#{i.capitalize}" +"!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)# code an argument here
   # Your code here
+  words.any? do |i|
+  i.length > 4
+
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese( )# code an argument here
+  # cheese_types = ["cheddar", "gouda", "camembert"]
+
+
 end
